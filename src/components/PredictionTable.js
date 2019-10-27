ed8f224bb5dc2b5b448c45bc67fa59302f8ecb77
Jiahao Li <reg@ljh.me>
2019-10-26 21:37:27 -0400

    Adds realtime countdown

--- src/components/PredictionTable.js
+++ src/components/PredictionTable.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import PredictionItem from './PredictionItem';
 
-function PredictionTable(props) {
-    const currentTime = Date.now();
-    const items = props.predictions.map((pred, index) => {
-        return <PredictionItem key={index} currentTime={currentTime} prediction={pred} />;
-    });
 
-    return (
-        <div>
-            <p>{props.predictions.length} predictions loaded.</p>
-            <table className='PredictionTable'>
-                {items}
-            </table>
-        </div>
-    );
-};
+class PredictionTable extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentTime: Date.now()
+        };
+    }
+
+    render() {
+        const items = this.props.predictions.map((pred, index) => {
+            return <PredictionItem key={index} currentTime={this.state.currentTime} prediction={pred} />;
+        });
+
+        return (
+            <div>
+                <p>{this.props.predictions.length} predictions loaded.</p>
+                <table className='PredictionTable'>
+                    {items}
+                </table>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({ currentTime: Date.now() });
+        }, 1000);
+    }
+}
 
 export default PredictionTable;
