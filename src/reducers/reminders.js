94ce92d203c19d04824e40590bd2249baf10ef6d
Jiahao Li <reg@ljh.me>
2019-12-15 21:55:46 -0500

    Fixes reminder status not re-rendering

--- src/reducers/reminders.js
+++ src/reducers/reminders.js
@@ -3,8 +3,9 @@ const INITIAL_STATE = {};
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case "REMINDERS_SET_STATUS":
-      state[action.payload.tripId] = action.payload.status;
-      return state;
+      var newState = {};
+      newState[action.payload.tripId] = action.payload.status;
+      return Object.assign(Object.assign({}, state), newState);
 
     default:
       return state;
